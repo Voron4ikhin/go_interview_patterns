c8176d7a3b12f9928559cdf1c93c85f6f5c86897
i.voronchikhin <i.voronchikhin@krediska.ru>
2026-09-18 23:28:38 +0300

    add semaphore realization

--- concurrency/semaphore/semaphore.go
+++ concurrency/semaphore/semaphore.go
@@ -1,1 +1,30 @@
 package semaphore
+
+import (
+	"context"
+	"errors"
+)
+
+type Semaphore chan struct{}
+
+func NewSemaphore(n int) Semaphore {
+	return make(Semaphore, n)
+}
+
+func (s Semaphore) Acquire(ctx context.Context) error {
+	select {
+	case s <- struct{}{}:
+		return nil
+	case <-ctx.Done():
+		return errors.New("semaphore: acquire canceled")
+	}
+}
+
+func (s Semaphore) Release() error {
+	select {
+	case <-s:
+		return nil
+	default:
+		return errors.New("semaphore: release without acquire")
+	}
+}
